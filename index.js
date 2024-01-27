1078c168f4587270075a97c9f90234dde87a40fc
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-27 17:24:17 +0100

    data.content.text !==

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const header = document.getElementById("header");
 const content = document.getElementById("content");
 
-
 fetch("https://api.blablagues.net/?rub=blagues")
   .then((res) => res.json())
   .then((data) => {
-    header.textContent = data.data.content.text_head
-    content.textContent = data.data.content.text ? data.data.content.text : data.data.content.text_hidden;
+    header.textContent = data.data.content.text_head;
+    content.textContent = data.data.content.text !== ""
+      ? data.data.content.text
+      : data.data.content.text_hidden;
   });
 
