a82bb81cef3b4c0c3db88b758bfc987a4ed939e1
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-27 17:26:17 +0100

    getJoke()

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const header = document.getElementById("header");
 const content = document.getElementById("content");
 
-fetch("https://api.blablagues.net/?rub=blagues")
+
+function getJoke() {
+  fetch("https://api.blablagues.net/?rub=blagues")
   .then((res) => res.json())
   .then((data) => {
     header.textContent = data.data.content.text_head;
     content.textContent = data.data.content.text !== ""
-      ? data.data.content.text
-      : data.data.content.text_hidden;
+    ? data.data.content.text
+    : data.data.content.text_hidden;
   });
-
+}
+getJoke()
