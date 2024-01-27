c70575ee1cb7c829c29b067abfd5d26dba173501
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-27 07:23:10 +0100

    Début du JS

---
+++ index.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("header");
+const content = document.getElementById("content");
+
+
+fetch("https://api.blablagues.net/?rub=blagues")
+  .then((res) => res.json())
+  .then((data) => {
+    header.textContent = data.data.content.text_head
+    content.textContent = data.data.content.text_head
+  });
+
